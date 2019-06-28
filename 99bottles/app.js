4e413106c411daa4231fd76f1c4281cd67d03916
Aaron Holland <aaronholla@gmail.com>
2019-06-27 22:11:28 -0400

    First Commit

---
+++ 99bottles/app.js
@@ -0,0 +1,11 @@
+const bottles = document.querySelectorAll('.bottle')
+const counters = document.querySelectorAll('.count')
+
+function drink(){
+  if(confirm("Take one down and pass it around?")){
+    this.classList.add('drank')
+    counters.forEach(counter => counter.textContent--)
+  }
+}
+
+bottles.forEach(bottle => bottle.addEventListener('click', drink))
---
+++ README.md
@@ -0,0 +1,5 @@
+# Wynfolio
+
+This is a portfolio site where I will be adding all of the projects that I work on at Wyncode.
+
+Currently it is a simple about me page with a link to play a game of 99 bottles of beer on the wall.
